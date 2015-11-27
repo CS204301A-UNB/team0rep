8520e3fd74ece976e9eadc9c6558eed7857633b9
Weichang Du <wdu@unb.ca>
2015-11-27 10:53:46 -0400

    test3

--- gittest/gittest/src/gittest/TestMain.java
+++ gittest/gittest/src/gittest/TestMain.java
@@ -4,7 +4,7 @@ public class TestMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello WDu!!");
+		System.out.println("Hello Weichang Du");
 	}
 
 }
